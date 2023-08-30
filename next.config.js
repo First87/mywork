fb0fbe149e3987b59caa14670ffe463acc41741c
First87 <apisitpanya5@gmail.com>
2023-08-31 00:03:19 +0700

    first commit

--- next.config.js
+++ next.config.js
@@ -2,15 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    // domains: ['drive.google.com'], // Add any other allowed domains as needed
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "drive.google.com",
-      },
-    ],
+    domains: ['drive.google.com'], // เพิ่มโดเมนที่อนุญาตให้โหลดรูปภาพมาจาก
   },
-  trailingSlash: true, // เพิ่ม trailing slash ให้กับ URL ที่สร้าง
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
